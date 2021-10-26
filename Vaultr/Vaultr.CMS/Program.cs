191a824de0e0aa8e6faeef9d2607ef72a93d79f1
Thomas Bleijendaal <t.bleijendaal@wearetriple.com>
2021-10-26 20:33:13 +0200

    Base rapidcms setup

---
+++ Vaultr/Vaultr.CMS/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Vaultr.CMS;
+using Vaultr.CMS.Authentication;
+using Vaultr.Core.Handlers;
+using Vaultr.Core.Providers;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddSingleton<IAuthorizationHandler, AllowAllAuthorizationHandler>();
+builder.Services.AddSingleton<AuthenticationStateProvider, ClientSpecifiedAuthenticationStateProvider>();
+
+builder.Services.AddRapidCMSWebAssembly(config =>
+{
+
+
+    config.SetCustomLoginScreen(typeof(LoginScreen));
+    // config.SetCustomLoginStatus(typeof(LoginScreen));
+});
+
+// TODO
+//builder.Services.AddMsalAuthentication(options =>
+//{
+//    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+//});
+
+await builder.Build().RunAsync();
---
+++ Vaultr/Vaultr.Core/Providers/ClientSpecifiedAuthenticationStateProvider.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Vaultr.Core.Providers
+{
+    public class ClientSpecifiedAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            return Task.FromResult(new AuthenticationState(new ClaimsPrincipal()));
+
+
+            var id = new ClaimsIdentity("anonymous");
+            id.AddClaim(new Claim(ClaimTypes.Name, "Anonymous"));
+
+            var principal = new ClaimsPrincipal(id);
+
+            return Task.FromResult(new AuthenticationState(principal));
+        }
+    }
+}
